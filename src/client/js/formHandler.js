41a2fadc54fe4c74b3a4f5a5f6dd511e09410482
Jacob Markovich <markovich.jacob@gmail.com>
2020-07-27 19:05:09 -0500

    Needing help with error

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,14 +2,25 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+    const url = document.getElementById('url').value
+    Client.checkForName(url)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+    fetch('//localhost:8081/sentiment-analysis', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ test: url })
+    })
     .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+    .then(data => {
+        document.getElementById('nlp-text').innerHTML = data.text;
+        document.getElementById('polarity').innerHTML = data.polarity;
+        document.getElementById('polarity_confidence').innerHTML = data.polarity_confidence;
+        document.getElementById('subjectivity').innerHTML = data.subjectivity;
+        document.getElementById('subjectivity_confidence').innerHTML = data.subjectivity_confidence;
     })
 }
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,49 @@
+const dotenv = require('dotenv')
+dotenv.config()
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+var aylien = require('aylien_textapi')
+var textapi = new aylien({
+    application_id: 'process.env.API_ID',
+    appplication_key: 'process.env.API_KEY'
+})
+const bodyParser = require('body-parser')
+const cors = require('cors')
+//const { text } = require('express')
 
 const app = express()
 
 app.use(express.static('dist'))
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-console.log(__dirname)
+//console.log(__dirname)
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+app.post('/sentiment-analysis', (req, res) => {
+    textapi.sentiment({ url: req.body.url }, (error, result) => {
+        if (error) {
+            console.log('Error during Aylien request')
+            res.send();
+            return;
+        }
+        console.log('Got Aylien result')
+        res.send(result)
+    })
+})
+
+module.exports = app
